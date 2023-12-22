27289bce7ac5a6925a7589ea505113e0bad99960
BartoszKmietowicz <artimo75@gmail.com>
2023-12-22 07:45:13 +0100

    added orders utility

--- routes/shop.js
+++ routes/shop.js
@@ -17,7 +17,7 @@ router.post('/cart', shopController.postCart);
 router.post('/cartDeleteItem', shopController.postCartDeleteProduct);
 
 // router.get('/orders', shopController.getOrders);
-// router.post('/createOrder', shopController.postOrder);
+router.post('/createOrder', shopController.postOrder);
 
 
 exports.routes = router;
